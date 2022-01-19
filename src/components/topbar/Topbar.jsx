59754718afd29726ecd2788cec10d88d545b55ac
Furrukh Khan <furrukhkhan10@gmail.com>
2022-01-18 21:53:54 -0500

    adding sidebar animation

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,8 @@
 import "./topbar.scss"
 import {Person,Mail} from "@material-ui/icons" 
-export default function Topbar(){
+export default function Topbar({menuOpen, setMenuOpen}){
     return(
-        <div className = "topbar">
+        <div className = {"topbar " + (menuOpen && "active")} >
             <div className = "wrapper">
                 <div className = "left">
                     <a href="#intro" className = "logo">Furrukh.</a>
@@ -16,8 +16,13 @@ export default function Topbar(){
                     </div>
                 </div>
                 <div className = "right">
+                    <div className = "hamburger" onClick = {()=> setMenuOpen(!menuOpen)}>
+                        <span className = "line1"></span>
+                        <span className = "line2"></span>
+                        <span className = "line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
+} 
