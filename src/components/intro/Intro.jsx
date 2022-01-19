1a8824a6851f6987593deb257e6b47b86eb8334e
Furrukh Khan <furrukhkhan10@gmail.com>
2022-01-19 11:35:00 -0500

    Adding Content to Intro. Using ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,35 @@
+import { useEffect , useRef } from "react"
 import "./intro.scss"
+import { init } from 'ityped'
 
 export default function Intro(){
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: false,
+            strings: ["Mulesoft", "Java Spring", "React", "AWS"],
+        });
+    },[]);
+
     return(
     <div className = "intro" id = "intro">
+        <div className="left">
+            <div className="imgContainer">
+                <img src="assets/dp.png" alt="" />
+            </div>
+        </div>
+        <div className="right">
+            <div className="wrapper">
+                <h2>Hi There, i'm</h2>
+                <h1>Furrukh Khan</h1>
+                <h3>Software Engineer <span ref={textRef}></span></h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
+        </div>
     </div>
     )
 }
